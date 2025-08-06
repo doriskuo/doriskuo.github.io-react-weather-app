fccbee43245bbc02a4cf738f098b099550970e9d
doriskuo <doriskuo0105@gmail.com>
2025-08-07 03:27:33 +0800

    adjust size

--- src/component/Weather.js
+++ src/component/Weather.js
@@ -19,38 +19,61 @@ const Weather = ({ data, city }) => {
     <div className="weatherbg">
       <div className="weatherbox">
         <div className="left">
-          <div className="iconbox">
-            {weathercode == "01" && <Sun />}
-            {["02", "03"].includes(weathercode) && <Suncloud />}
-            {["04", "05", "06", "07"].includes(weathercode) && (
-              <Cloud className="cloud-in-weather" />
-            )}
-            {["08", "09", "10", "11", "12", "13", "14", "29", "30"].includes(
-              weathercode
-            ) && <Rain className="rain-in-weather" />}
-            {["15", "16", "17", "18", "22", "33", "34"].includes(
-              weathercode
-            ) && <Thunderrain />}
-            {["21"].includes(weathercode) && <Thundersun />}
+          <div className="top-in-left">
+            <div className="left-in-lefttop">
+              <p className="temperature">
+                {data[0].Time[0].ElementValue[0].Temperature}˚C
+              </p>
+              <h3 className="city">{city}</h3>
+              <p className="date">
+                {dayjs(data[0].Time[0].StartTime).format("M月D日 HH:mm")}
+              </p>
+              <p className="precipitation">
+                降雨機率
+                {data[11].Time[0].ElementValue[0].ProbabilityOfPrecipitation}%
+              </p>
+            </div>
+
+            <div className="right-in-lefttop">
+              <div className="iconbox">
+                {weathercode == "01" && <Sun className="sun-in-weather" />}
+                {["02", "03"].includes(weathercode) && (
+                  <Suncloud className="suncloud-in-weather" />
+                )}
+                {["04", "05", "06", "07"].includes(weathercode) && (
+                  <Cloud className="cloud-in-weather" />
+                )}
+                {[
+                  "08",
+                  "09",
+                  "10",
+                  "11",
+                  "12",
+                  "13",
+                  "14",
+                  "29",
+                  "30",
+                ].includes(weathercode) && <Rain className="rain-in-weather" />}
+                {["15", "16", "17", "18", "22", "33", "34"].includes(
+                  weathercode
+                ) && <Thunderrain className="thunderrain-in-weather" />}
+                {["21"].includes(weathercode) && (
+                  <Thundersun className="thundersun-in-weather" />
+                )}
+              </div>
+              <h6 className="overview">
+                {data[12].Time[0].ElementValue[0].Weather}
+              </h6>
+            </div>
           </div>
-          <div className="city">{city}</div>
-          <div className="date">
-            {dayjs(data[0].Time[0].StartTime).format("M月D日 HH:mm")}
-          </div>
-          <div className="overview">
-            {data[12].Time[0].ElementValue[0].Weather}
-          </div>
-          <div className="precipitation">
-            降雨機率
-            {data[11].Time[0].ElementValue[0].ProbabilityOfPrecipitation}%
-          </div>
-          <div className="temperature">
-            {data[0].Time[0].ElementValue[0].Temperature}˚C
-          </div>
-          <div className="tip">
-            {data[14].Time[0].ElementValue[0].WeatherDescription}
+
+          <div className="bottom-in-left">
+            <h6 className="tip">
+              {data[14].Time[0].ElementValue[0].WeatherDescription}
+            </h6>
           </div>
         </div>
+
         <div className="right">
           <div className="topbox">
             <Weektemperature
--- src/component/Weektemperature.js
+++ src/component/Weektemperature.js
@@ -13,23 +13,23 @@ const Weektemperature = ({ Weekweathertemperatures }) => {
     return tem.ElementValue[0].Temperature;
   });
   const points = temps
-    .map((temp, index) => `${index * 50 + 50},${150 - temp * 10 + 250}`)
+    .map((temp, index) => `${index * 80 + 50},${150 - temp * 15 + 420}`)
     .join(" ");
 
   return (
     <div className="temperaturebox">
-      <svg viewBox="0 0 400 150">
+      <svg viewBox="0 0 600 220">
         {daytemperatures.map((t, i) => {
           let tcode = t.ElementValue[0].Temperature;
           return (
             <>
               <circle
-                cx={i * 50 + 50}
-                cy={150 - tcode * 10 + 250}
-                r="3"
+                cx={i * 80 + 50}
+                cy={150 - tcode * 15 + 420}
+                r="4"
                 key={i}
               ></circle>
-              <text x={i * 50 + 50} y={150 - tcode * 10 + 240}>
+              <text x={i * 80 + 50} y={150 - tcode * 15 + 400}>
                 {tcode}˚C
               </text>
             </>
